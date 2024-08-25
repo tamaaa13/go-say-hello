fdbbf3b8e25314c42eb44f03bdfaf0f56d8a26b6
Tegar Aditya Pratama <tegarpratama811@gmail.com>
2024-08-25 23:33:03 +0700

    Update SayHello with parameter name

--- say_hello.go
+++ say_hello.go
@@ -3,6 +3,6 @@ package go_say_hello
 // ingin membuat module tinggal bikin di git lalu beri tag dgn cara :
 // tulis di terminal git tag v1.0.0(contoh) sehabis semuanya di commit
 
-func SayHello() string {
-	return "Hello World"
+func SayHello(name string) string {
+	return "Hello" + name
 }
